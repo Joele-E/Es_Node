f6c063d71407326bc447420ee20e03d59092ef9a
Joele-E <esposito.joelebiz@gmail.com>
2023-11-13 19:51:04 +0100

    exercise done

--- queries.sql
+++ queries.sql
@@ -1,3 +1,16 @@
-BEGIN TRANSACTION;
-DELETE FROM Books WHERE book_id = 101;
-ROLLBACK;
+SELECT * FROM titanic WHERE survived=1 AND sex='female' AND age>30 ;
+
+/*====================================================================*/
+SELECT AVG(age) FROM titanic WHERE survived=0 AND sex='male';
+
+/*====================================================================*/
+
+SELECT * FROM titanic WHERE fare BETWEEN 20 AND 50 AND embarked='C';
+
+/*====================================================================*/
+
+SELECT COUNT(*) FROM titanic WHERE pclass=1 AND survived=1;
+
+/*====================================================================*/
+
+SELECT * FROM titanic WHERE embarked='C' AND fare>75;
